309183bf601b5512d892bb379892d47e9d9a30df
PrajwalHs2609 <prajwalhs4india@gmail.com>
2024-06-07 11:20:33 +0530

    mail changes

--- src/Components/Contacts/ContactForm.jsx
+++ src/Components/Contacts/ContactForm.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Contacts.css";
 const ContactForm = () => {
+  const [data, setData] = useState({
+    name: "",
+    phone: "",
+    email: "",
+    requirement: "",
+  });
+  let { name, phone, email, requirement } = data;
+  let handleChange = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    setData({ ...data, [name]: value });
+  };
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    let { name, phone, email, requirement } = data;
+    const mailtoLink = `mailto:hello@buildurspace.com?subject=Message from ${name}&body=${encodeURIComponent(
+      `Name: ${name}\nPhone: ${phone}\nEmail: ${email}\n\n${requirement}`
+    )}`;
+    window.location.href = mailtoLink;
+  };
   return (
     <div className="contactFormContainer">
-      <form action="">
+      <form action="" onSubmit={handleSubmit}>
         <legend>
           <h1>
             Lets <span>Talk</span>
@@ -13,32 +33,52 @@ const ContactForm = () => {
           <table>
             <tr>
               <td>
-                <input type="text" placeholder="NAME" />
+                <input
+                  type="text"
+                  placeholder="NAME"
+                  onChange={handleChange}
+                  value={name}
+                  name="name"
+                />
               </td>
             </tr>
             <tr>
               <td>
-                <input type="text" placeholder="Phone No" />
+                <input
+                  type="text"
+                  placeholder="Phone No"
+                  onChange={handleChange}
+                  value={phone}
+                  name="phone"
+                />
               </td>
             </tr>
             <tr>
               <td>
-                <input type="text" placeholder="Email" />
+                <input
+                  type="text"
+                  placeholder="Email"
+                  onChange={handleChange}
+                  value={email}
+                  name="email"
+                />
               </td>
             </tr>
             <tr>
               <td>
                 <textarea
-                  name=""
+                  name="requirement"
+                  value={requirement}
                   id=""
                   cols={30}
                   rows={3}
                   placeholder="Your requirement"
+                  onChange={handleChange}
                 ></textarea>
               </td>
             </tr>
             <div className="contactFormButton">
-            <button type="submit">Submit</button>
+              <button type="handleSubmit">Submit</button>
             </div>
           </table>
         </fieldset>
