3b664ea7b66918c11616b3435202b62c93b5fc5c
PrajwalHs2609 <prajwalhs4india@gmail.com>
2024-06-09 14:38:45 +0530

    update

--- src/Components/Construction/PackageConstruction.jsx
+++ src/Components/Construction/PackageConstruction.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import "../PackagePage/Package.css";
 import PackageMenu from "../PackagePage/PackageMenu";
 import PackageList from "../PackagePage/PackageList";
+import PackageHead from './../PackagePage/PackageHead';
 
 const PackageConstruction = () => {
   return (
     <div className="packageMainContainer">
+      <PackageHead/>
       <PackageMenu />
       <PackageList />
     </div>
