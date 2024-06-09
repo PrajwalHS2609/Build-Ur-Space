8943200fc935bb7bef1ec903f084413a1ec84048
PrajwalHs2609 <prajwalhs4india@gmail.com>
2024-06-09 14:02:46 +0530

    about update

--- src/Components/HomePage/HomeOur/HomeOur1.jsx
+++ src/Components/HomePage/HomeOur/HomeOur1.jsx
@@ -3,24 +3,21 @@ import "./HomeOur1.css";
 const HomeOur1 = (props) => {
   return (
     <div className="our1Container">
-      <div className="our1Content1">
-        <div className="our1head">
-          <h1>
-            {props.h1} <span>{props.h2}</span>
-          </h1>
-        </div>
-        <div className="our1Item1">
-          <p>
-           {props.txt}
-          </p>
-        </div>
+      <div className="our1head">
+        <h1>
+          {props.h1} <span>{props.h2}</span>
+        </h1>
       </div>
-      <div className="our1Content2">
-        <div className="our1Item2">
-          <img
-            src={props.img}
-            alt=""
-          />
+      <div className="our1Wrapper">
+        <div className="our1Content1">
+          <div className="our1Item1">
+            <p>{props.txt}</p>
+          </div>
+        </div>
+        <div className="our1Content2">
+          <div className="our1Item2">
+            <img src={props.img} alt="" />
+          </div>
         </div>
       </div>
     </div>
--- src/Components/HomePage/HomeOur/HomeOur2.jsx
+++ src/Components/HomePage/HomeOur/HomeOur2.jsx
@@ -2,23 +2,25 @@ import React from "react";
 import "./HomeOur2.css";
 const HomeOur2 = (props) => {
   return (
-    <div className="our2Container">
-      <div className="our2Content2">
-        <div className="our2Item2">
-          <img src={props.img} alt="" />
-        </div>
-      </div>
-      <div className="our2Content1">
+      <div className="our2Container">
         <div className="our2head">
           <h1>
             {props.h1} <span>{props.h2}</span>
           </h1>
         </div>
-        <div className="our2Item1">
-          <p>{props.txt}</p>
+        <div className="our2Wrapper">
+          <div className="our2Content1">
+            <div className="our2Item1">
+              <p>{props.txt}</p>
+            </div>
+          </div>
+          <div className="our2Content2">
+            <div className="our2Item2">
+              <img src={props.img} alt="" />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
