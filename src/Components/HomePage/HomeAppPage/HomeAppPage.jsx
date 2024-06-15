47b53ba1fbef1f0cdea57ddb68ebcb9e55f1e548
PrajwalHs2609 <prajwalhs4india@gmail.com>
2024-06-15 20:28:52 +0530

    changes

--- src/Components/HomePage/HomeAppPage/HomeAppPage.jsx
+++ src/Components/HomePage/HomeAppPage/HomeAppPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+  import React from "react";
 import "./HomeAppPage.css";
 import HomeAppTxt from "./HomeAppTxt";
 import houseApp from "./../../../Images/Mobile App.png";
--- src/Components/NavBar/Logo.jsx
+++ src/Components/NavBar/Logo.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./NavBar.css";
 import logo from "./../../Images/mainLogo.png";
+import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
     <div className="navBarLogoContainer">
-      <img src={logo} alt="" />
+     <Link to={"/"}> <img src={logo} alt="" /></Link>
     </div>
   );
 };
