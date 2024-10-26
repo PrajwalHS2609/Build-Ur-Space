8a71749ce4ab0a500545b8e7998fd233afc8c7ee
PrajwalHs2609 <prajwalhs4india@gmail.com>
2024-10-26 11:00:29 +0530

    imgblog

--- src/Components/Blog/BlogHeader.jsx
+++ src/Components/Blog/BlogHeader.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-import "./Blog.css"
+import "./Blog.css";
 const BlogHeader = () => {
-  return <div className="blogHeader">
-
-  </div>;
+  return (
+    <div className="blogHeader">
+      <img
+        src="https://img.freepik.com/free-photo/woman-showing-with-one-hand-mini-house-real-state-concept-ai-generative_123827-24098.jpg?uid=R153459844&ga=GA1.1.798303094.1721912336&semt=ais_hybrid"
+        alt=""
+      />
+    </div>
+  );
 };
 
 export default BlogHeader;
